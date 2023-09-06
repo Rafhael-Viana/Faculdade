7e78f307f214a39e184004cb578bf254bdf297d6
Rafhael-Viana <vnarafha@gmail.com>
2023-09-06 11:36:42 -0300

    Atividade Python - Introdução e Métodos de manipulação de arrays(listas)

---
+++ int-progr-estrut-python/ex05.py
@@ -0,0 +1,28 @@
+# Crie uma lista com números de 1 a 10
+l = [1,2,3,4,5,6,7,8,9,10]
+# Remova o elemento na posição 2
+l.pop(2)
+# Remova o elemento de valor 5
+l.remove(5)
+# Acrescente 11 e 12 no final da lista
+l.append(11)
+l.append(12)
+# Acrescente o 0 no início da lista
+l.insert(0,0)
+# Imprima a lista no final.
+print(l)
+
+# Atv 2
+
+# Crie uma lista com 5 números aleatórios
+l1 = [8,4,9,12,0]
+# Crie outra lista com 3 números de 5 a 7
+l2 = [7,5,6]
+# Concatene as duas listas
+l2.extend(l1)
+# Imprima a lista concatenada em ordem
+l2.sort( )
+# Remova o  numero do índice 4
+l2.pop(4)
+# Imprima a lista no final.
+print(l2)
