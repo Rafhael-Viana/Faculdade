f94b4931ddb6fffce3c36c2c6ef0bdc508c865c4
Rafhael-Viana <vnarafha@gmail.com>
2023-09-20 15:02:35 -0300

    Trabalho (Sorteio) - Exercício em Python
    O programa recebe 10 nomes a serem sorteados como possíveis ganhadores, gera uma lista jogo contendo 20 números e gera uma lista de sorteio contendo 6 números. Os ganhadores são sorteados aleatoriamente, quando todos os números da lista sorteio estiverem contidos na lista jogo.

---
+++ int-progr-estrut-python/trabalho.py
@@ -0,0 +1,37 @@
+import random
+
+c = 0
+nomes = []
+
+for x in range (0,10):
+    nome = input("Digite um nome: ")
+    nomes.append(nome)
+
+while (c < 6): # loop para a verificação mais rápida do ganhador.
+    sorteio = (random.sample(range(0,60), 6))
+    jogo = (random.sample(range(0,60), 20))
+
+
+    sorteio.sort()
+    jogo.sort()
+
+    print("--------------------------------")
+    print(f"\n{sorteio}")
+    print(f"\n{jogo}\n")
+
+    for i in jogo:
+        for x in sorteio:
+            if (x == i):
+                c += 1
+    print(f"\nQuantidade de Acertos: {c}")
+
+    if (c == 6):
+        print(f"\nGanhador: {nomes[random.randint(0,10)]}\n")
+        break
+    else:
+        print("\nNão foi dessa vez! O prêmio acumulou!\n")
+        c = 0
+
+
+
+# print(random.sample(range(0,100), 4))
