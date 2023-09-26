67a0c5e2ce0b1ee68372ce16520b643e7d1b0beb
Rafhael-Viana <vnarafha@gmail.com>
2023-09-26 01:28:02 -0300

    Refactor: Melhorias no Código

--- int-progr-estrut-python/trabalho.py
+++ int-progr-estrut-python/trabalho.py
@@ -5,12 +5,12 @@ import random
 c = 0
 nomes = []
 
-for x in range (0,10):
+for x in range (10):
     nome = input("Digite um nome: ")
     nomes.append(nome)
 
-while (c < 6): # loop para a verificação mais rápida do ganhador.
-    # A utilização de randint resulta na múltipla repetição de números. Daí a troca para a função sample.
+while c < 6: # loop para a verificação mais rápida do ganhador.
+    # A função randint resulta na múltipla repetição de números. Daí a troca para a função sample.
     sorteio = (random.sample(range(0,60), 6))
     jogo = (random.sample(range(0,60), 20))
 
@@ -23,14 +23,13 @@ while (c < 6): # loop para a verificação mais rápida do ganhador.
 
     for i in jogo:
         for x in sorteio:
-            if (x == i):
+            if x == i:
                 c += 1
     print(f"\nQuantidade de Acertos: {c}")
 
-    if (c == 6):
-        print(f"\nGanhador: {nomes[random.randint(0,10)]}\n")
+    if c == 6:
+        print(f"\nGanhador: {nomes[random.randint(0,len(nomes))]}\n")
         break
-    else:
-        print("\nNão foi dessa vez! O prêmio acumulou!\n")
-        c = 0
-        
+    print("\nNão foi dessa vez! O prêmio acumulou!\n")
+    c = 0
+    
