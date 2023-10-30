ac5d171bdbcbb1b59cf2e2041771b45ac9178403
Rafhael-Viana <vnarafha@gmail.com>
2023-10-30 10:08:08 -0300

    Atividade Prática Avaliativa - Menu Hotel v1.0

---
+++ int-progr-estrut-python/atv_avaliativa2.py
@@ -0,0 +1,133 @@
+# #? importa as bibliotecas
+from datetime import datetime as dt
+import math
+
+# Definir lista de quartos e de cadastros(reservas)
+l_quartos = []
+cadastros = []
+
+#? Implementar o loop do menu
+while True:
+    
+    print("\n---------------------------------------------")
+    print("|                    MENU                   |")
+    print("---------------------------------------------")
+    print("|                                           |")
+    print("| 1 - Cadastrar Quartos                     |")
+    print("| 2 - Consultar Quartos                     |")
+    print("| 3 - Criar Reserva                         |")
+    print("| 4 - Consultar Reserva                     |")
+    print("| 5 - Cancelar Reserva                      |")
+    print("| 6 - Fechar Hospedagem                     |")
+    print("| 7 - Sair                                  |")
+    print("|                                           |")
+    print("---------------------------------------------\n")
+
+    try:
+        option = int(input("Selecione uma opção: "))
+
+        match option:
+
+            #? Cadastro de Quartos
+            case 1:
+                print("\nIniciando cadastro de quartos...\n")
+                # declaração da função cadastro de quartos
+                def initCadastro(quartos):
+                    control = ""
+                    while control != 0:
+                        quarto = int(input("Cadastre seu quarto. Pressione '0' para sair... "))
+                        if quartos.count(quarto) > 0:
+                            print("\nQuarto já cadastrado...\n")
+                        else:
+                            quartos.append(quarto)
+                            control = quartos[len(quartos) - 1]
+                            # print(control)
+                            if control == 0:
+                                quartos.pop(len(quartos) - 1)
+                    return quartos
+                #chamada da função
+                print(f'\nQuartos:{initCadastro(l_quartos)}')
+
+            #? Consulta de Quartos
+            case 2:
+                if not l_quartos:
+                    print("\nNenhum quarto cadastrado...")
+                else:
+                    print(f'\nQuartos Disponíveis: {l_quartos}')
+
+            #? Criar Reserva
+            case 3:
+                def createReservation(cliente,numero):
+                    if l_quartos.count(numero) == 0:
+                        print("\nQuarto não registrado ou já cadastrado. Por favor, repita a operação...")
+                    else:
+                        cliente = {
+                            "cliente": cliente,
+                            "data-entrada" : dt.now(),
+                            "numero_quarto": numero,
+                            "status" : "Ocupado"
+                        }
+                        cadastros.append(cliente)
+                        l_quartos.pop(l_quartos.index(numero))
+                        print("\nReserva Concluída com Sucesso.\n")
+                createReservation(input("Nome: ").capitalize(),int(input("Numero do Quarto: ")))
+
+            #? Consultar Quartos
+            case 4:
+                print("Iniciando consulta...\n")
+                controle = ""
+                while controle != "q":
+                    num_quarto = int(input("Digite o número do quarto: "))
+                    def getClient(quarto):
+                        for i,cliente in enumerate(cadastros):
+                            if cadastros[i].get("numero_quarto") == quarto:
+                                return cliente
+                    info = getClient(num_quarto)
+                    if info is not None:
+                        print(f'\nCliente: {info.get("cliente")}')
+                        print(f'Data-Entrada: {info.get("data-entrada").strftime("%d-%m-%Y %H:%M:%S")}')
+                        print(f'N° Quarto: {info.get("numero_quarto")}')
+                        print(f'Status: {info.get("status")}\n')
+                    else:
+                        print('\nCliente: ')
+                        print('Data-Entrada: ')
+                        print(f'N° Quarto: {num_quarto}')
+                        print('Status: Disponível\n')
+                    controle = input("Pressione 'q' para sair ou 'Enter' para continuar...\n")
+
+            #? Cancelar Reserva
+            case 5:
+                print("\nIniciando cancelamento...\n")
+                def cancelReservation(cliente):
+                    for i,cadastro in enumerate(cadastros):
+                        if cadastro.get("cliente") == cliente:
+                            cadastros.pop(i)
+                            return "\nReserva Cancelada."
+                print(cancelReservation(input("Digite o nome do cliente: ").capitalize()))
+
+            #? Fechar Hospedagem
+            case 6:
+                print("\nFechando hospedagem...\n")
+                reserva = input("Digite o nome do cliente: ").capitalize()
+                def pegarClient(cliente):
+                    for i,cadastro in enumerate(cadastros):
+                        if cadastros[i].get("cliente") == cliente:
+                            return cadastro
+                info = pegarClient(reserva)
+                saida = dt.strptime(input("Data-Saída (dd/mm/aaaa hh:mm:ss): "),"%d/%m/%Y %H:%M:%S")
+                tempo = saida - info.get("data-entrada")
+                valor = math.ceil((tempo.total_seconds()/3600)/24) * 50
+
+                for item,hospede in enumerate(cadastros):
+                    if hospede.get("cliente") == reserva:
+                        cadastros.pop(item)
+
+                print("\n-------------------------------------------")
+                print(f"Estadia: {tempo}\nValor à pagar: {valor}R$")
+
+            #? Parar o programa
+            case 7:
+                break
+
+    except ValueError:
+        print("\nOpção Inválida")
