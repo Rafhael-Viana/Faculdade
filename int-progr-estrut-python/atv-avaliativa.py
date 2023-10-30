98dfaa75f7d006748cddbae51ed6e201ff98755b
Rafhael-Viana <vnarafha@gmail.com>
2023-10-30 10:06:11 -0300

    Atividade Prática Avaliativa - Estacionamento v1.0

---
+++ int-progr-estrut-python/atv-avaliativa.py
@@ -0,0 +1,24 @@
+# Adiciona (importa) a biblioteca math
+import math
+
+#? Entrada e Saida
+ent = int(input("Digite a hora de ent: ")) + int(input("Digite os minutos de ent: ")) / 60
+saida = int(input("Digite a hora de saida: ")) + int(input("Digite os minutos da saida: ")) / 60
+
+#? implementar o formato 24h
+
+tempo = saida - ent
+if ent > saida:
+    tempo = (24 - ent) + saida
+
+#? imprimir o tempo de permanência
+print("\n-----------------------------------")
+print(f'{math.floor(tempo)} : {((tempo - math.floor(tempo)) * 60):.0f}')
+
+#? calcular o pagamento
+conta = math.ceil(tempo) * 5 + math.ceil(tempo) * 1
+if tempo <= 2:
+    conta = math.ceil(tempo) * 5
+
+#? Imprimir o valor da conta
+print(f'Valor: {conta} R$\n')
